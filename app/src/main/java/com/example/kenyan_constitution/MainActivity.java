c9e3d90a01a9b135cfe2def70932bb896f17cb6d
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-16 13:45:50 +0300

    use an intent on my button to make it navigate to following activity when clicked

--- app/src/main/java/com/example/kenyan_constitution/MainActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kenyan_constitution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         mButtonMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
+                Intent intent = new Intent(MainActivity.this, KenyanConstitutionActivity.class);
+                startActivity(intent);
             }
         });
 
