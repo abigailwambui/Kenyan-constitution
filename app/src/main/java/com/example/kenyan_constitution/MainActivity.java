c22e4bf5450191122ad434b2270116d0a1dcc30c
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-16 13:21:28 +0300

    set button in mainactivity.java

--- app/src/main/java/com/example/kenyan_constitution/MainActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.kenyan_constitution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+        private Button mButtonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonMain = (Button) findViewById(R.id.buttonMain);
+
     }
 }
