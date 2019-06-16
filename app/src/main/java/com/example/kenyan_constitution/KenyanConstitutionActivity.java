bc79f084732f72e3911f8f9691abeb65688ea343
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-16 15:20:57 +0300

    add new activity called laws and functionality to the button of the second activity

--- app/src/main/java/com/example/kenyan_constitution/KenyanConstitutionActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/KenyanConstitutionActivity.java
@@ -2,13 +2,33 @@ package com.example.kenyan_constitution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class KenyanConstitutionActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private Button mSubmitKenyanConstitution;
+    private Button mButtonKenyanConstitution;
+    private EditText mNameEditText;
+    private EditText mEmailEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kenyan_constitution);
+        setContentView(R.layout.activity_constitution_kenyan);
+        mNameEditText = (EditText) findViewById(R.id.nameEditText);
+        mEmailEditText = (EditText) findViewById(R.id.emailEditText);
+        mButtonKenyanConstitution = (Button) findViewById(R.id.buttonKenyanConstitution);
+        mButtonKenyanConstitution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(KenyanConstitutionActivity.this, LawsActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/kenyan_constitution/LawsActivity.java
@@ -0,0 +1,14 @@
+package com.example.kenyan_constitution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LawsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laws);
+    }
+}
