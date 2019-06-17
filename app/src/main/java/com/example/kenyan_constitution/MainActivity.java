250a0209d352c28034d639ed5b0c357556d337cf
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-17 12:01:15 +0300

    apply downloaded fonts

--- app/src/main/java/com/example/kenyan_constitution/MainActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.kenyan_constitution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
         private Button mButtonMain;
+        private TextView mTextViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextViewMain = (TextView) findViewById(R.id.textViewMain);
+        Typeface walkWayFont = Typeface.createFromAsset(getAssets(), "fonts/Walkway_Bold.ttf");
+        mTextViewMain.setTypeface(walkWayFont);
         mButtonMain = (Button) findViewById(R.id.buttonMain);
         mButtonMain.setOnClickListener(new View.OnClickListener() {
             @Override
