9a6a0aeaf2efda1bdf9e66e9e1bafcc0b371d429
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-17 11:24:31 +0300

    define our laws in LawsActivity.java

--- app/src/main/java/com/example/kenyan_constitution/LawsActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/LawsActivity.java
@@ -2,13 +2,20 @@ package com.example.kenyan_constitution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LawsActivity extends AppCompatActivity {
+    private TextView mLawsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laws);
+        mLawsTextView = (TextView) findViewById(R.id.lawsTextView);
+        Intent intent = getIntent();
+        String laws = intent.getStringExtra("laws");
+        mLawsTextView.setText("Kenyan laws click to get elaborate details: " + laws);
     }
 }
