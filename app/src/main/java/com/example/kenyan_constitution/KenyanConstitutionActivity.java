19bdeb066a4594eea38fea5ebe6eb8524690f304
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-17 12:51:15 +0300

    add an array list to display the preamble first

--- app/src/main/java/com/example/kenyan_constitution/KenyanConstitutionActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/KenyanConstitutionActivity.java
@@ -3,11 +3,15 @@ package com.example.kenyan_constitution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class KenyanConstitutionActivity extends AppCompatActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -15,11 +19,15 @@ public class KenyanConstitutionActivity extends AppCompatActivity {
     private Button mButtonKenyanConstitution;
     private EditText mNameEditText;
     private EditText mEmailEditText;
+    private TextView mTextViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constitution_kenyan);
+        mTextViewMain = (TextView) findViewById(R.id.textViewMain);
+        Typeface walkWayFont = Typeface.createFromAsset(getAssets(), "fonts/Walkway_Oblique.ttf");
+        mTextViewMain.setTypeface(walkWayFont);
         mNameEditText = (EditText) findViewById(R.id.nameEditText);
         mEmailEditText = (EditText) findViewById(R.id.emailEditText);
         mButtonKenyanConstitution = (Button) findViewById(R.id.buttonKenyanConstitution);
--- app/src/main/java/com/example/kenyan_constitution/LawsActivity.java
+++ app/src/main/java/com/example/kenyan_constitution/LawsActivity.java
@@ -3,19 +3,44 @@ package com.example.kenyan_constitution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LawsActivity extends AppCompatActivity {
     private TextView mLawsTextView;
+    private ListView mListView;
+    private String[] laws = new String[] {"We the people of Kenya-","ACKNOWLEDGING", "HONOURING",
+            "PROUD", "RESPECTFUL", "COMMITTED", "RECOGNISING",
+            "EXERCISING", "ADOPT, ENACT", "GOD BLESS KENYA"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laws);
+
+        mListView = (ListView) findViewById(R.id.listView1);
         mLawsTextView = (TextView) findViewById(R.id.lawsTextView);
+        Typeface walkWayFont = Typeface.createFromAsset(getAssets(), "fonts/Walkway_Oblique_SemiBold.ttf");
+        mLawsTextView.setTypeface(walkWayFont);
+        mLawsTextView = (TextView) findViewById(R.id.lawsTextView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, laws);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(LawsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String laws = intent.getStringExtra("laws");
-        mLawsTextView.setText("Kenyan Laws by LOF Kenya. Click to get elaborate details: " + laws);
+        mLawsTextView.setText("PREAMBLE");
     }
 }
